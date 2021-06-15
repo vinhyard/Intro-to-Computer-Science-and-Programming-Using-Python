33ae78d53be8c800a616ae1cb51f4cca2973a118
vinhyard <vinhyard1@gmail.com>
2021-06-15 13:25:25 -0500

    Update Unit 2 Exercise Guess my number.py

--- Unit 2 Exercise Guess my number.py
+++ Unit 2 Exercise Guess my number.py
@@ -5,70 +5,30 @@
 #When the user enters something invalid, you should print out a message to the user explaining you did not understand their input.
 
 
-high = 50
-low = 50//2
-
-i = 'l' 
-u = 'h'
-
-#high and low is for the range 0-100.
+#At the start the highest the number could be is 100 and the lowest is 100.
+chad = 100
+doug = 0
+answ = False
 
 print('Please think of a number between 0 and 100!')
-print('Is your secret number', high, '?')
-chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
-while i in chad:
-    if i == 'l':
-        high += low
-        print('Is your secret number', high, '?')
-        chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
-        if chad == 'l':
-            low = low//2
-            high += low
-            print('Is your secret number', high, '?')
-            chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
-            if chad == 'l':
-                high += 6
-                print('Is your secret number', high, '?')
-                chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
-                while chad == 'l':
-                    high += 1
-                    print('Is your secret number ', high, '?')
-                    chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
-                    
-                
-            else: break    
-        else: break
-        
-    else:break
 
-while u in chad:
-    if u =='h':    
-        high -= low
-        print('Is your secret number', high, '?')
-        chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
-        if chad == 'h':
-            low = low//2
-            high -= low
-            print('Is your secret number', high, '?')
-            chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
-            if chad == 'h':
-                high += 6
-                print('Is your secret number', high, '?')
-                chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
-                while chad == 'h':
-                    high -= 1
-                    print('Is your secret number ', high, '?')
-                    chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
-            else: break
-        else:break            
-                   
-    else:break
-if chad == 'c': print('Game over. Your secret number was:', high)
-else:print('Sorry, I did not understand your input.')    
+
+#Loop until we gues it correctly
+while not answ:
+    #Bisection search: guess the midpoint between our current high and low guesses
+    guess = (chad + doug)//2
+    print('Is your secret answer', guess, '?')
+    palantir = input('Enter "h" to indicate too high. Enter "l" to indicate too low. Enter "c" to indicate I guessed correctly.')
+
+
+    if palantir == 'c':
+        answ = True
+
+
+    elif palantir == 'h':
+        chad = guess   
     
-       
-
-
-
-
-
+    elif palantir == 'l':
+        doug = guess
+    else: print('Sorry, I did not understand your input')
+print('Game over. Your secret number was:', guess)
