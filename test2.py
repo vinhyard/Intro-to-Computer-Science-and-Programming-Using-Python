32b009a50e889dc8b76af47e03fae1d887481481
vinhyard <vinhyard1@gmail.com>
2021-06-15 09:04:48 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -6,16 +6,14 @@ high1 = 50/2
 
 print('Please think of a number between 0 and 100!')
 print('Is your secret number', high, '?') 
-chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
 while high < 100:
+    chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
     if chad == 'h':
         high //=2
         print('Is your secret number', high, '?') 
-        chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
     elif chad == 'l':
         high += (high//2)
         print('Is your secret number', high, '?')
-        chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
     elif chad == 'c':
         break
     
