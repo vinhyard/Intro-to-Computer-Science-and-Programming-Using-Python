78732e0a64b072d6547ab12f3b3213901363d50b
vinhyard <vinhyard1@gmail.com>
2021-06-15 12:51:17 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,5 +1,4 @@
 high = 50
-high1 = 50/2
 
 
 
@@ -9,38 +8,57 @@ print('Is your secret number', high, '?')
 while high <= 50:
     chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
     if chad == 'h':
-        high //=2
-        print('Is your secret number', high, '?') 
-        while 0 <= high <= 5 or 19 <= high <= 23  :
-            chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
-            if chad == 'h':
-                high -= 1
-                print('Is your secret number', high, '?')
-            elif chad == 'l':
-                high += 1
-                print('Is your secret number', high, '?')
-            elif chad == 'c':
-                print('Game over. Your secret number was:', high) 
-    elif chad == 'l':  
-        if high == 18:
+        if high == 18 or high == 37:
   
             if chad == 'l':
                 high += (high//6)
                 print('Is your secret number', high, '?')
+                continue
             elif chad == 'h':
                 high -= (high//6)   
                 print('Is your secret number', high, '?')  
-        while 7 <= high < 12 or 15 <= high <= 17 or 19 <= high <= 24:
-            
-            chad = input('Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly.')
+                continue
+        if 0 <= high <= 5 or 7 <= high < 12 or  15 <= high <= 17 or 19 <= high <= 24:
+
             if chad == 'h':
                 high -= 1
                 print('Is your secret number', high, '?')
+                continue
             elif chad == 'l':
                 high += 1
                 print('Is your secret number', high, '?')
+                continue
             elif chad == 'c':
                 print('Game over. Your secret number was:', high)
+            else: break
+        high //=2
+        print('Is your secret number', high, '?')                  
+    elif chad == 'l':  
+        if high == 18 or high == 37:
+  
+            if chad == 'l':
+                high += (high//6)
+                print('Is your secret number', high, '?')
+                continue
+            elif chad == 'h':
+                high -= (high//6)   
+                print('Is your secret number', high, '?')  
+                continue
+        if 7 <= high < 12 or 15 <= high <= 17 or 19 <= high <= 24 or 0 <= high <= 5:
+            
+          
+            if chad == 'h':
+                high -= 1
+                print('Is your secret number', high, '?')
+                continue
+            elif chad == 'l':
+                high += 1
+                print('Is your secret number', high, '?')
+                continue
+            else: 
+                chad == 'c'
+                print('Game over. Your secret number was:', high)
+                break
        
         
         high += (high//2)
@@ -48,5 +66,5 @@ while high <= 50:
         
     elif chad == 'c':
         break
-    
+    else: print('Sorry, I don"t understand your input')
 print('Game over. Your secret number was: ', high,)
