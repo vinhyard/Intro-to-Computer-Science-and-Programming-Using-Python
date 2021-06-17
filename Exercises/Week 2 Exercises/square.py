d580c74566ca77e74798ffe1884ba9cdcef98782
vinhyard <vinhyard1@gmail.com>
2021-06-16 21:46:40 -0500

    Organization

---
+++ Exercises/Week 2 Exercises/square.py
@@ -0,0 +1,18 @@
+#Write a Python function, square, that takes in one number and returns the square of that number.
+
+#This function takes in one number and returns one number.
+
+
+def square(x):
+    """
+x: int or float
+
+    """
+
+
+    x = x**2
+
+    return(x)
+
+
+print(square(9))
