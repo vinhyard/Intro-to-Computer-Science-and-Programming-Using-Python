33f7bbee4e32360377155ce6df475bb63a4fe9b4
vinhyard <vinhyard1@gmail.com>
2021-06-18 02:45:26 -0500

    Update Problem Set 2 Problem 1.py

--- Problem Sets/Problem Set 2/Problem Set 2 Problem 1.py
+++ Problem Sets/Problem Set 2/Problem Set 2 Problem 1.py
@@ -14,3 +14,10 @@
 #Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
 #Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
 #Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
+
+balance = 42
+annualInterestRate = .2
+monthlyPaymentRate = .04
+
+
+
